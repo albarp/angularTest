d04b161701e6943beeb1bbd0a0a0dfc7cda10224
Alberto A <alberto.arpini@fmc-ag.com>
2018-11-18 10:03:39 +0100

    added nested component Star

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import{FormsModule} from '@angular/forms';
 import { ConvertoToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
+    StarComponent,
     ConvertoToSpacesPipe
   ],
   imports: [
