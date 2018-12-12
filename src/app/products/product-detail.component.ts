7d3217711ea927eeef4c4314bb34617cd9c8c145
Alberto A <alberto.arpini@fmc-ag.com>
2018-12-12 21:21:04 +0100

    added route parameters

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   // selector: 'app-product-detail', Non serve perchè non lo richiamiamo nell'html, ma ci navighiamo con il routing
@@ -9,9 +10,21 @@ import { IProduct } from './product';
 export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product detail';
   product: IProduct;
-  constructor() { }
+  constructor(private acticatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    const id = +this.acticatedRoute.snapshot.paramMap.get('id');
+    this.pageTitle = `${id}`;
+    this.product = {
+      'productId': 1,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-0011',
+      'releaseDate': 'March 19, 2016',
+      'description': 'Leaf rake with 48-inch wooden handle.',
+      'price': 19.95,
+      'starRating': 3.2,
+      'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
+    };
   }
 
 }
