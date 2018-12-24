f58c6f08b342128c30fb81804cb3223eea686c9b
Alberto A <alberto.arpini@fmc-ag.com>
2018-12-24 21:32:54 +0100

    Child component input and output

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
-import { NgModel } from '@angular/forms';
+import { CriteriaComponent } from '../shared/criteria/criteria.component';
 
 @Component({
     // selector: 'app-pm-products', non serve se lo raggiungiamo con il binding
@@ -11,10 +11,11 @@ import { NgModel } from '@angular/forms';
 export class ProductListComponent implements OnInit, AfterViewInit {
 
     pageTitle = 'Product List!';
+    showImage = false;
+    includeDetail = true;
 
     imageWidth: Number = 50;
     imageMargin: Number = 2;
-    showImage = false;
     errorMessage = '';
 
     // Comodo per essere avvisati ogni volta che il valore viene cambiato
@@ -34,6 +35,9 @@ export class ProductListComponent implements OnInit, AfterViewInit {
     @ViewChild('filterRef') filterRef: ElementRef; // per recuperare la template reference variable
     // @ViewChild(NgModel) filterInput: NgModel; // diventa un event emitter
 
+    @ViewChild(CriteriaComponent) filterComponent: CriteriaComponent;
+    parentListFilter: string; // è quello che verrà letto dal figlio
+
     products: IProduct[] = [];
 
       constructor(private productService: ProductService) {
@@ -44,6 +48,10 @@ export class ProductListComponent implements OnInit, AfterViewInit {
       }
 
       performFilter(filterBy: string): IProduct[] {
+
+        if (!filterBy) {
+          return this.products;
+        }
 
         filterBy = filterBy.toLocaleLowerCase();
 
@@ -63,20 +71,32 @@ export class ProductListComponent implements OnInit, AfterViewInit {
 
       ngOnInit(): void {
         this.productService.getProducts().subscribe(products => {
+          if (products) {
           this.products = products;
-          this.filteredProducts = this.products;
+          this.filteredProducts = this.performFilter(this.parentListFilter);
+          }
         },
         error => this.errorMessage = <any>error);
       }
 
       ngAfterViewInit(): void {
+
+        this.parentListFilter = this.filterComponent.listFilter;
+
+        // Tutto spostato nel componente CriteriaComponent
         // Se usiamo la templare reference variable
-        if (this.filterRef.nativeElement) { // Meglio controllare perchè, andando direttamente sul DOM, potrebbe essere che alcuni metodi manchino
-          this.filterRef.nativeElement.focus();
-        }
+        // if (this.filterRef.nativeElement) { // Meglio controllare perchè, andando direttamente sul DOM, potrebbe essere che alcuni metodi manchino
+        //   this.filterRef.nativeElement.focus();
+        // }
 
         // this.filterInput.valueChanges.subscribe(
         //   () => this.performFilter(this.listFilter)
         // );
+      }
+
+      onFilterValueChanged(newFilter: string): void {
+        if (newFilter) {
+          this.filteredProducts = this.performFilter(newFilter);
+        }
       }
 }
--- src/app/shared/criteria/criteria.component.ts
+++ src/app/shared/criteria/criteria.component.ts
@@ -1,18 +1,43 @@
-import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit, Input, OnChanges, SimpleChanges, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-criteria',
   templateUrl: './criteria.component.html',
   styleUrls: ['./criteria.component.css']
 })
-export class CriteriaComponent implements OnInit, AfterViewInit {
+export class CriteriaComponent implements OnInit, AfterViewInit, OnChanges {
 
-  listFilter: string;
+  @Input() displayDetail: boolean;
+  @Input() hitNumber: number;
+  @Output() filterValueChanged: EventEmitter<string> =
+      new EventEmitter<string>();
+
+  private _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(val: string) {
+    this._listFilter = val;
+    this.filterValueChanged.emit(val);
+  }
+
+  hitMessage: string;
+
   @ViewChild('filterElement') filterElementRef: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // Scatta ogni qual volta cambia una varibile marchiata come
+  // @Input, oppure [ngModel]
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['hitNumber'] && !changes['hitNumber'].currentValue) {
+      this.hitMessage = 'No matches';
+    } else {
+      this.hitMessage = 'Hit: ' + this.hitNumber;
+    }
   }
 
   ngAfterViewInit(): void {
